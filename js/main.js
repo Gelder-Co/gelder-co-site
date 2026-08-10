f50d6c6774c161a56b93972eedb04114415c40d3
onestopdesignshop <de8e888@gmail.com>
2026-08-10 12:13:57 -0500

    Log message when DOM is ready
    Added event listener for DOMContentLoaded.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+// GELDER & Co. - Main JavaScript
+console.log('GELDER & Co. loaded');
+
+// Add custom functionality here
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('DOM ready');
+});
 
